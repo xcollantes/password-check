ef9fc3a9b5a9f28ada59456dd1db8d00133e50a2
xcollantes <xcollantes@users.noreply.github.com>
2019-03-16 22:37:29 -0700

    Test

--- PwnedMe.py
+++ PwnedMe.py
@@ -4,11 +4,29 @@
 # file: PwnedMe.py
 # purpose: Check if input password has been compromised via haveibeenpwned.com API 
 
+import requests, hashlib, pandas,
+
+API = 'https://api.pwnedpasswords.com/'
+PARAMS = { 'range': }
 
 def main():
+  inputPw('HelloWorld')
   
+    
+	
+# inputPw: intake password as plain text and encode 
+#          into SHA-1 as required by API
+def inputPw(password):
+  hashP = hashlib.sha1(password)
+  print(hashP)
+  
+def pwned(hash):
+  try:
+    response = requests.get(API, params=hash)
+  except:
 
-
+  
+  return response
 
 
 if __name__=='__main__':
